52eaa5b9728e638d055142274ba39beebebc3b8b
wiamadanan <wiamadnan20@gmail.com>
2024-02-04 18:28:51 +0000

    Update args

--- pretrain_graph_model.py
+++ pretrain_graph_model.py
@@ -25,35 +25,28 @@ log_dir_name = f"{args['model_name']}--{augment_str}--{args['lr']}--{args['batch
 args['tensorboard_dir'] = os.path.join(args['tensorboard_dir'], log_dir_name)
 args['save_dir'] = os.path.join(args['save_dir'], log_dir_name)
 
-class AttributeDict(dict):
-    def __init__(self, *args, **kwargs):
-        super(AttributeDict, self).__init__(*args, **kwargs)
-        self.__dict__ = self
-
-args = AttributeDict(args)
-
 # Initialize logging and TensorBoard writer
-os.makedirs(args.save_dir, exist_ok=True)
-writer = SummaryWriter(log_dir=args.tensorboard_dir)
+os.makedirs(args['save_dir'], exist_ok=True)
+writer = SummaryWriter(log_dir=args['tensorboard_dir'])
 
 # Load dataset
 gt = np.load("./data/token_embedding_dict.npy", allow_pickle=True).item()
 dataset = GraphDatasetPretrain(root='./data/', gt=gt, split='cids')
-train_dataset, val_dataset, test_dataset = split_dataset(dataset, args.train_data_percent)
+train_dataset, val_dataset, test_dataset = split_dataset(dataset, args['train_data_percent'])
 train_loader = build_loader(args, train_dataset, "train")
 val_loader = build_loader(args, val_dataset, "val")
 test_loader = build_loader(args, test_dataset, "test")
 
 # Model and optimizer
 model = GraphEncoder(
-    num_node_features=args.feat_dim,
-    nout=args.nout,
-    nhid=args.nhid,
-    graph_hidden_channels=args.graph_hidden_channels,
-    heads=args.heads
+    num_node_features=args['feat_dim'],
+    nout=args['nout'],
+    nhid=args['nhid'],
+    graph_hidden_channels=args['graph_hidden_channels'],
+    heads=args['heads']
 ).to(device)
 
-optimizer = optim.Adam(model.parameters(), lr=args.lr)
+optimizer = optim.Adam(model.parameters(), lr=args['lr'])
 
 scheduler = ReduceLROnPlateau(
     optimizer,
@@ -68,8 +61,8 @@ def run(epoch, mode, dataloader):
         model.train()
     else:
         model.eval()
-    if args.loss in ['infonce', 'jensen_shannon']:
-        contrastive_fn = eval(args.loss + "()")
+    if args['loss'] in ['infonce', 'jensen_shannon']:
+        contrastive_fn = eval(args['loss'] + "()")
     else: 
         contrastive_fn = contrastive_loss
 
@@ -93,7 +86,7 @@ def run(epoch, mode, dataloader):
     writer.add_scalar('Learning rate', current_lr, epoch)
     return avg_loss
 
-for epoch in range(args.epochs):
+for epoch in range(args['epochs']):
     train_loss = run(epoch, "train", train_loader)
     val_loss = run(epoch, "val", val_loader)
     print(f"Epoch {epoch}, Train Loss: {train_loss:.3f}, Val Loss: {val_loss:.3f}")
@@ -108,18 +101,18 @@ for epoch in range(args.epochs):
     is_best = val_loss < best_val_loss if epoch > 0 else True
     if is_best:
         best_val_loss = val_loss
-        torch.save(model.state_dict(), os.path.join(args.save_dir, "best_model.pth"))
+        torch.save(model.state_dict(), os.path.join(args['save_dir'], "best_model.pth"))
 
     torch.save({
         'epoch': epoch,
         'model_state_dict': model.state_dict(),
         'optimizer_state_dict': optimizer.state_dict(),
         'loss': train_loss,
-    }, os.path.join(args.save_dir, "last_model.pth"))
+    }, os.path.join(args['save_dir, "last_model.pth"))
 
 # Load best model for testing
-model.load_state_dict(torch.load(os.path.join(args.save_dir, "best_model.pth")))
-test_loss = run(args.epochs, "test", test_loader)
+model.load_state_dict(torch.load(os.path.join(args['save_dir, "best_model.pth")))
+test_loss = run(args['epochs, "test", test_loader)
 print(f"Test Loss: {test_loss:.3f}")
 
 # Close TensorBoard writer
